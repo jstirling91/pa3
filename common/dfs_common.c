6df6ca68cc2f4dfbc1c48d062e989602d2d15844
jstirling91 <jstirling91@gmail.com>
2013-11-26 17:25:40 -0500

    TEST0

--- common/dfs_common.c
+++ common/dfs_common.c
@@ -95,7 +95,7 @@ void send_data(int socket, void* data, int size)
     int result;
     while(bytesWrite < size){
         result = write(socket, data + bytesWrite, size - bytesWrite);
-        if(reuslts < 1){
+        if(results < 1){
             printf("ERROR: did not send\n");
             return;
         }
