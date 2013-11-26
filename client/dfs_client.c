48a1d23aa91e600f382e3adf5979cdf89c740e66
jstirling91 <jstirling91@gmail.com>
2013-11-26 13:38:49 -0500

    TEST0

--- client/dfs_client.c
+++ client/dfs_client.c
@@ -87,11 +87,11 @@ dfs_system_status *get_system_info(int namenode_socket)
 	//TODO: get the response
 	dfs_system_status *response;
     data = (char*)malloc(sizeof(request));
-    receive_data(namenode_socket, data, sizeof(request));
+    receive_data(namenode_socket, data, sizeof(response));
     memcpy(&response, data, sizeof(response));
     
     
-    printf("SUCCESS: got system_status response\n");
+    printf("SUCCESS: got system_status response %d\n", response.datanode_num);
 
 	return response;		
 }
