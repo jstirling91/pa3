1f8d0eb9ca1102f03ffdc5bc0ee24df3460cfbc7
jstirling91 <jstirling91@gmail.com>
2013-11-26 14:43:31 -0500

    TEST0

--- namenode/dfs_namenode.c
+++ namenode/dfs_namenode.c
@@ -32,7 +32,7 @@ int mainLoop(int server_socket)
 //        char data[300];
         receive_data(client_socket, (void *)data, sizeof(request));
         memcpy(&request, data, sizeof(request));
-        send_data(client_socket, (void *)data, sizeof(request))
+        send_data(client_socket, (void *)data, sizeof(request));
         
 //		requests_dispatcher(client_socket, request);
 		close(client_socket);
