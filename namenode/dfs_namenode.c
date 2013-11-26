6e0273fe1b6a759e52fca40fc79f0fc34391d043
jstirling91 <jstirling91@gmail.com>
2013-11-26 15:32:48 -0500

    TEST0

--- namenode/dfs_namenode.c
+++ namenode/dfs_namenode.c
@@ -168,10 +168,10 @@ void get_system_information(int client_socket, dfs_cm_client_req_t request)
 	//TODO:fill the response and send back to the client
 	dfs_system_status response;
     response.datanode_num = 2;
-    char *data = (char*)malloc(sizeof(dfs_system_status));
-    memcpy(&response, data, sizeof(dfs_system_status));
-    send_data(client_socket, (void *)data, sizeof(dfs_system_status));
-    free(data);
+//    char *data = (char*)malloc(sizeof(dfs_system_status));
+//    memcpy(, sizeof(dfs_system_status));
+    send_data(client_socket, &response, sizeof(dfs_system_status));
+//    free(data);
 }
 
 int get_file_update_point(int client_socket, dfs_cm_client_req_t request)
