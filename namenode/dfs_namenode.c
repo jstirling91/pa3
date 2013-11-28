f9869fa2eea4d600f26e369e17c5219b77630fe6
jstirling91 <jstirling91@gmail.com>
2013-11-27 22:48:54 -0500

    TEST1

--- namenode/dfs_namenode.c
+++ namenode/dfs_namenode.c
@@ -151,7 +151,7 @@ int get_file_receivers(int client_socket, dfs_cm_client_req_t request)
         blockNode.block_id = next_data_node_index;
         memcpy(&blockNode.loc_ip, dnlist[next_data_node_index%dncnt]->ip, sizeof(dnlist[next_data_node_index%dncnt]->ip));
         blockNode.loc_port = dnlist[next_data_node_index%dncnt]->port;
-        (*file_image)->block_list[next_data_node_index] = &blockNode;
+        memcpy((*file_image)->block_list[next_data_node_index], &blockNode, sizeof(blockNode));
     }
 
 	dfs_cm_file_res_t response;
