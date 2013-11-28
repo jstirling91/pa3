e1be934721a7e96071871f5636b1ad7d30e2a3de
jstirling91 <jstirling91@gmail.com>
2013-11-28 09:42:27 -0500

    TEST1

--- namenode/dfs_namenode.c
+++ namenode/dfs_namenode.c
@@ -91,15 +91,17 @@ int register_datanode(int heartbeat_socket)
 		{
 			//TODO: fill dnlist
 			//principle: a datanode with id of n should be filled in dnlist[n - 1] (n is always larger than 0)
-            dfs_datanode_t dnode;
-            dnode.dn_id = n;
-            strcpy(dnode.ip, inet_ntoa(serv_addr.sin_addr));
-//            dnode.ip = *inet_ntoa(addr.sin_addr);
-            dnode.port = datanode_status.datanode_listen_port;
-            dncnt++;
-            dnlist[n - 1] = &dnode;
-            printf("PORT: %d, %d\n", dnlist[n - 1]->port, n);
-			safeMode = 0;
+            if(dnlist[n - 1] == NULL){
+                dfs_datanode_t dnode;
+                dnode.dn_id = n;
+                strcpy(dnode.ip, inet_ntoa(serv_addr.sin_addr));
+                //            dnode.ip = *inet_ntoa(addr.sin_addr);
+                dnode.port = datanode_status.datanode_listen_port;
+                dncnt++;
+                dnlist[n - 1] = &dnode;
+                printf("PORT: %d, %d\n", dnlist[n - 1]->port, n);
+                safeMode = 0;
+            }
 		}
 		close(datanode_socket);
 	}
